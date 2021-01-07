55a6b845a128b9107c041abea3a2f097b1799508
LeoNunes21 <77120617+LeoNunes21@users.noreply.github.com>
2021-01-07 16:32:03 -0300

    Add files via upload

---
+++ Código.py
@@ -0,0 +1,9 @@
+print("Qual é o seu nome?")
+nome = input()
+print("Qual é a sua idade?")
+idade = input()
+print(f" Seja bem-vindo(a) a ByLearn, {nome}!")
+
+
+
+print(f" O ususário {nome} tem {idade} anos.")
