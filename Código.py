80729d28faf0ebb91ce907c109931be1f5571139
LeoNunes21 <77120617+LeoNunes21@users.noreply.github.com>
2021-01-07 16:36:21 -0300

    Update Código.py

--- Código.py
+++ Código.py
@@ -2,8 +2,7 @@ print("Qual é o seu nome?")
 nome = input()
 print("Qual é a sua idade?")
 idade = input()
-print(f" Seja bem-vindo(a) a ByLearn, {nome}!")
-
-
 
 print(f" O ususário {nome} tem {idade} anos.")
+
+print(f" Seja bem-vindo(a) a ByLearn, {nome}!")
